4852ad3404356cda1aa2ebed516d548f4750799d
mkramers <mattkramers@gmail.com>
2019-11-25 23:23:49 -0500

    Improve multi-episode support

--- series_parser.py
+++ series_parser.py
@@ -36,9 +36,19 @@ class SeriesApiSearcher(ApiSearcher):
 
 class SeriesRenamePathBuilder(RenamePathBuilder):
 
+    def prepend_episode(self, episode_number):
+        return f"E{episode_number:02}"
+
     def build_rename_path(self, result):
         directory = path.join(result.name, f"Season {result.season:02}")
-        filename = f"{result.name} - S{result.season:02}E{result.episode:02} - {result.episode_title}.mkv"
+
+        if hasattr(result.episode, "__len__"):
+            episodes = map(self.prepend_episode, result.episode)
+            episode_string = "-".join(episodes)
+        else:
+            episode_string = self.prepend_episode(result.episode)
+
+        filename = f"{result.name} - S{result.season:02}{episode_string} - {result.episode_title}.mkv"
         return path.join(directory, filename)
 
 
