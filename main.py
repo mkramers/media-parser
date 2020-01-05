e2a3af366c0465b26f6fe350edff50ae60a73829
mk <mattkramers@gmail.com>
2020-01-05 08:21:09 -0500

    Improve cleaning

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from media_file_cleaner import MediaFileCleaner
+from remove_empty_dirs import removeEmptyFolders
 from series_media_parser import SeriesMediaParser
 import os
 
@@ -15,17 +16,38 @@ def main():
     outputDir = 'F:\videos\TV Shows'
     deleteDir = 'C:\seed\_delete_me'
     
-    ignored_extensions = "^.\\\\Sample.*|^sample.*|(^.*\.(nfo|NFO|sfv|SFV|exe|EXE|txt|TXT|rar|RAR|r[0-9]{2}|R[0-9]{2})$)"
+    ignored_extensions = "^(.*)\\Sample\\.*|(^.*\.(nfo|NFO|sfv|SFV|exe|EXE|txt|TXT|rar|RAR|r[0-9]{2}|R[0-9]{2})$)"
 
     parser = SeriesMediaParser()
     cleaner = MediaFileCleaner(ignored_extensions, deleteDir)
 
-    for root, dirs, files in os.walk(rootDir):
-        for file in files:
-            if not cleaner.try_clean_file(file):
-                print("renaming " + file)
-                result = parser.get_rename_path(file)
-                print(result)
+    files = getListOfFiles(rootDir);
+    for file in files:
+        if not cleaner.try_clean_file(file, rootDir):
+            print("renaming " + file)
+            # result = parser.get_rename_path(file)
+            # print(result)
+
+    # clean empty dirs
+    removeEmptyFolders(rootDir, False)
+
+
+def getListOfFiles(dirName):
+    # create a list of file and sub directories
+    # names in the given directory
+    listOfFile = os.listdir(dirName)
+    allFiles = list()
+    # Iterate over all the entries
+    for entry in listOfFile:
+        # Create full path
+        fullPath = os.path.join(dirName, entry)
+        # If entry is a directory then get the list of files in this directory
+        if os.path.isdir(fullPath):
+            allFiles = allFiles + getListOfFiles(fullPath)
+        else:
+            allFiles.append(fullPath)
+
+    return allFiles
 
 
 main()
--- media_file_cleaner.py
+++ media_file_cleaner.py
@@ -1,4 +1,5 @@
 import os
+import pathlib
 import re
 from os import path
 
@@ -11,23 +12,26 @@ class MediaFileCleaner(FileCleanerBase):
         self.ignored_extensions = ignored_extensions
         self.delete_dir = delete_dir
 
-    def try_clean_file(self, filename):
+    def try_clean_file(self, filename, rootDir):
         _, extension = os.path.splitext(filename)
+
+        relative_path = os.path.relpath(filename, rootDir)
 
         should_clean = False
 
-        if re.search(self.ignored_extensions, filename):
+        if re.search(self.ignored_extensions, relative_path):
             should_clean = True
 
         if should_clean:
-            dest = path.join(self.delete_dir, filename);
+            dest = path.join(self.delete_dir, relative_path);
             self.clean_file(filename, dest)
 
         return should_clean
 
     def clean_file(self, filename, dest):
-        # print("cleaning: " + dest)
-        print("skip")
-        # os.rename(filename, dest)
+        print("move: " + filename + "\n\tto\n\t" + dest )
+        dir_path = os.path.dirname(os.path.realpath(dest))
+        pathlib.Path(dir_path).mkdir(parents=True, exist_ok=True)
+        os.rename(filename, dest)
 
 
